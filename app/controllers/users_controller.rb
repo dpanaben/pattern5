1222e28264228532f100ac68d8498d595142330f
David <dpan.orz@gmail.com>
2016-07-16 15:40:08 +0800

    add is_admin to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,22 @@
 class UsersController < ApplicationController
+  before_action :is_admin
+
   def index
-    if current_user && current_user.admin?
-      @users = User.all
-    else
-      redirect_to root_url, notice: "You are not authorized"
-    end
+    @users = User.all
   end
 
   def show
-    if current_user && current_user.admin?
-      @user = User.find(params[:id])
-    else
-      redirect_to root_url, notice: "You are not authorized"
-    end
+    @user = User.find(params[:id])
   end
 
   def update
-    if current_user && current_user.admin?
-      @user = User.find(params[:id])
-      if @user.update_attributes(secure_params)
-        redirect_to users_path, :notice => "User updated."
-      else
-        redirect_to users_path, :alert => "Unable to update user."
-      end
+    @user = User.find(params[:id])
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, :notice => "User updated."
     else
-      redirect_to root_url, notice: "You are not authorized"
+      redirect_to users_path, :alert => "Unable to update user."
     end
   end
-
 
   def destroy
     user = User.find(params[:id])
@@ -36,6 +25,12 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def is_admin
+    unless current_user.admin?
+      redirect_to root_url, notice: "You are not authorized to that area."
+    end
+  end
 
   def secure_params
     params.require(:user).permit(:role)
