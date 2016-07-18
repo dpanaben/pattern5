b6780d14c8801fde067906f930f987e2810ebc79
David <dpan.orz@gmail.com>
2016-07-18 11:54:58 +0800

    move protect_from_forgery with: :exception to the first line of application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :set_locale
   before_action :set_paper_trail_whodunnit
 
-  protect_from_forgery with: :exception
 
   private
   # def set_locale
