119e76eca76f089ba88e9237530028d03f7b1992
David <dpan.orz@gmail.com>
2016-08-03 17:15:43 +0800

    ok

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,11 @@
 class Portfolio < ApplicationRecord
   has_many :sanitizes, dependent: :nullify
   has_many :posts, dependent: :nullify
+  belongs_to :cop, class_name: "User", foreign_key: :user_id
   after_create :after_create
 
   def after_create
-    if self.user?
+    if self.cop.user?
       self.sanitizes.create!(:match => '\s\,|\,|\,\s', :result => '，', :description => '把 , 改成全形符號')
     end
   end
